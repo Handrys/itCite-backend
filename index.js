98db40c714e3ceb6bf61e456d04e072c6fb01b5c
Handrys <handry.hard228@gmail.com>
2022-12-28 12:42:14 +0200

    upd link mongoDB

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect('mongodb+srv://admin:admin123@itblog.ujcnuey.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
